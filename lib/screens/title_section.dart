c5b750127fd37e330e1a34280a3f5484460e809a
Eric Mahasi <mahasieric@gmail.com>
2022-02-14 15:49:27 +0300

    Add instructions to detect disease page

---
+++ lib/screens/title_section.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:leafly/services/style.dart';
+
+class TitleSection extends SliverFixedExtentList {
+  TitleSection(String title, double height, {Key? key})
+      : super(
+          key: key,
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB((0.32 * height), 0, 0, 0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                          fontSize: (0.6 * height),
+                          fontFamily: 'SFBold',
+                          color: darkGreen),
+                    ),
+                  ),
+                ],
+              );
+            },
+            childCount: 1,
+          ),
+          itemExtent: height,
+        );
+}
